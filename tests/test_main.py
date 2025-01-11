041b60af61b50cca474ce1c43dcb23f10d66c081
nex84 <schwartzmann.a@gmail.com>
2025-01-11 12:33:27 +0100

    Refactor tests to utilize common and config modules for improved clarity

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,30 @@
 import src.main as main
+import src.modules.common as common
+import src.modules.config as config
 import yaml
 
+
 def test_get_formatted_datetime():
-  assert main.get_formatted_datetime()
+  assert common.get_formatted_datetime()
+
 
 def test_read_config():
-  config = main.read_config("src/config")
-  assert yaml.dump(config)
+  services_config = config.current_config.config["services"]
+  assert yaml.dump(services_config)
+
 
 def test_checkHTTPSuccess():
   assert main.check_serviceHTTP("https://www.google.com")
+
 
 def test_checkHTTPFail():
   res = main.check_serviceHTTP("https://www.unknownurlfqdn.com")
   assert res == False
 
+
 def test_checkTCPSuccess():
   assert main.check_serviceTCP("www.google.com", 443)
+
 
 def test_checkTCPFail():
   res = main.check_serviceTCP("www.unknownurlfqdn.com", 10)
