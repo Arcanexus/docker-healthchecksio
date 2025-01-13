a49deca3c6c266d2d6f672a742128a8dbf4a3f0a
nex84 <schwartzmann.a@gmail.com>
2025-01-13 19:16:48 +0100

    Refactor tests to use checks module for service health checks

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-import src.main as main
+import src.modules.checks as checks
 import src.modules.common as common
-import src.modules.config as config
+# import src.modules.config as config
 import yaml
 
 
@@ -8,24 +8,29 @@ def test_get_formatted_datetime():
   assert common.get_formatted_datetime()
 
 
-def test_read_config():
-  services_config = config.current_config.config["services"]
-  assert yaml.dump(services_config)
+# def test_get_config_value():
+#   value = config.current_config.config.get("some_key")
+#   assert value is not None
+
+# def test_read_config():
+#   # services_config = config.current_config.config["services"]
+#   current_config = config.read_config("src/config/")
+#   assert yaml.dump(current_config)
 
 
 def test_checkHTTPSuccess():
-  assert main.check_serviceHTTP("https://www.google.com")
+  assert checks.check_serviceHTTP("https://www.google.com")
 
 
 def test_checkHTTPFail():
-  res = main.check_serviceHTTP("https://www.unknownurlfqdn.com")
+  res = checks.check_serviceHTTP("https://www.unknownurlfqdn.com")
   assert res == False
 
 
 def test_checkTCPSuccess():
-  assert main.check_serviceTCP("www.google.com", 443)
+  assert checks.check_serviceTCP("www.google.com", 443)
 
 
 def test_checkTCPFail():
-  res = main.check_serviceTCP("www.unknownurlfqdn.com", 10)
+  res = checks.check_serviceTCP("www.unknownurlfqdn.com", 10)
   assert res == False
