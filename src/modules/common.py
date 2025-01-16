8c33dcf1cb1facaa82794d0f5ce7c1b317f48e9d
nex84 <schwartzmann.a@gmail.com>
2025-01-16 09:35:15 +0100

    Remove unused threading import and debug print function; simplify to_camel_case function

--- src/modules/common.py
+++ src/modules/common.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-import threading
 import re
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -20,21 +20,16 @@ class bcolors:
     ERROR = RED
     CRITICAL = RED
 
+
 # Function to get the current date and time formatted as "31-Dec-2023 19:34"
 def get_formatted_datetime():
     now = datetime.now()
     return now.strftime("%d-%b-%Y %H:%M")
 
-def printdebug(msg, debug=False):
-    if debug:
-        current_datetime = get_formatted_datetime()
-        thread_name = threading.current_thread().name
-        thread_id = threading.get_ident()
-        print(f"{bcolors.OKBLUE}{current_datetime} - [DEBUG] - [{thread_name}-{thread_id}] - {msg}{bcolors.ENDC}")
 
-# Helper function to convert a string to camelCase and truncate it to 8 characters
+# Helper function to convert a string to camelCase
 def to_camel_case(s):
     s = re.sub(r'[^a-zA-Z0-9]', ' ', s)
     s = ''.join(word.capitalize() for word in s.split())
-    # return s[:8]
+    s = s[0].lower() + s[1:]
     return s
